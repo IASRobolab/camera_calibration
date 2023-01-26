81abf8f94acdc3a779f22081b0464f4116ca56a1
Federico Rollo <rollo.f96@gmail.com>
2023-01-26 22:37:16 +0100

    added license header

--- camera_calibration_lib/cameras_extrinsic_calibration.py
+++ camera_calibration_lib/cameras_extrinsic_calibration.py
@@ -1,3 +1,25 @@
+'''----------------------------------------------------------------------------------------------------------------------------------
+# Copyright (C) 2022
+#
+# author: Federico Rollo, Fabio Amadio
+# mail: rollo.f96@gmail.com
+#
+# Institute: Leonardo Labs (Leonardo S.p.a - Istituto Italiano di tecnologia)
+#
+# This file is part of camera_calibration. <https://github.com/IASRobolab/camera_calibration>
+#
+# camera_calibration is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# camera_calibration is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License. If not, see http://www.gnu.org/licenses/
+---------------------------------------------------------------------------------------------------------------------------------'''
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from tqdm import tqdm
